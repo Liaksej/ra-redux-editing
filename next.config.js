25243164c0e09c46259b78b7b8396e796ae6b041
Liaksej <tarusov.liaksej@gmail.com>
2023-10-27 16:15:24 +0200

    Update build script, suppress basePath, enable darkMode, modify file paths and design
    Modified `package.json` to address GitHub Pages’ default behavior to skip files that begin with underscore using '.nojekyll' file. Updated `next.config.js` to address the path issues cropping up due to the varying environments. Added 'darkMode' to `tailwind.config.ts`, updated file paths and added various design elements including colors, borderRadius and animations. Removed unnecessary import in `page.tsx`. Changes aim to improve the project's configurability and the application's overall aesthetics.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/re-redux-editing/" : "",
+  // basePath: process.env.NODE_ENV === "production" ? "/re-redux-editing" : "",
 };
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import configureStore from "@/redux/store";
 import { Store } from "redux";
 import { AppState } from "@/redux/state";
 import { App } from "@/components/App";
-import Head from "next/head";
 
 const store: Store<AppState> = configureStore();
 
